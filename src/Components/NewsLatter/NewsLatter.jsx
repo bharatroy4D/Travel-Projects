9c5ecdcec405a44828ff2e832cf67a546a2780be
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-04-01 17:46:04 +0600

    newslatter desing in complite

--- src/Components/NewsLatter/NewsLatter.jsx
+++ src/Components/NewsLatter/NewsLatter.jsx
@@ -3,21 +3,22 @@ import { IoMailOpenOutline } from 'react-icons/io5';
 
 const NewsLatter = () => {
     return (
-        <div>
+        <div className='flex justify-between items-center max-w-7xl px-20 my-20 py-10 bg-blue-50'>
             {/* div-1 */}
             <div>
                 <div className='flex gap-5 items-center'>
                     <IoMailOpenOutline className='text-9xl text-blue-600' />
                     <div>
                         <h4 className='text-2xl font-semibold'>Get the latest news and offers</h4>
-                        <h1 className='text-3xl font-bold'>Subscribe to our newsletter</h1>
+                        <h1 className='text-3xl font-bold pt-2'>Subscribe to our newsletter</h1>
                     </div>
                 </div>
             </div>
 
             {/* div-2 */}
-            <div>
-
+            <div className='flex border rounded-r w-[40%]'>
+                <input type="email" name="email" id="" placeholder='Enter your email' className=' w-full py-3 px-3 outline-none ' />
+                <button className=' py-3 px-3 border rounded-r bg-blue-600 text-white'>Subscribe</button>
             </div>
         </div>
     );
