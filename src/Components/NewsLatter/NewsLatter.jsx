f5165203e6a3fbff0a684864d9408d2f63e4a68c
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-04-01 17:15:52 +0600

    newslatter of 50% react icon and title added

--- src/Components/NewsLatter/NewsLatter.jsx
+++ src/Components/NewsLatter/NewsLatter.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { IoMailOpenOutline } from 'react-icons/io5';
 
 const NewsLatter = () => {
     return (
         <div>
-            hdjfjopdkf
+            {/* div-1 */}
+            <div>
+                <div className='flex gap-5 items-center'>
+                    <IoMailOpenOutline className='text-9xl text-blue-600' />
+                    <div>
+                        <h4 className='text-2xl font-semibold'>Get the latest news and offers</h4>
+                        <h1 className='text-3xl font-bold'>Subscribe to our newsletter</h1>
+                    </div>
+                </div>
+            </div>
+
+            {/* div-2 */}
+            <div>
+
+            </div>
         </div>
     );
 };
