ae294efcc04069a73123006fdce498f5dd9da10d
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-04-02 18:37:43 +0600

    places component is connected

--- src/Components/Places/Places.jsx
+++ src/Components/Places/Places.jsx
@@ -9,7 +9,7 @@ const Places = () => {
         .then(data=>setPlaces(data))
     },[])
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-10'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 pb-15 gap-8 px-10'>
             {
                 places.map(place=><Place key={place.id} place={place} ></Place>)
             }
--- src/Components/PopularTours/PopularTours.jsx
+++ src/Components/PopularTours/PopularTours.jsx
@@ -21,7 +21,7 @@ const PopularTours = () => {
                     <FaArrowCircleRight className='hover:text-blue-600 transform hover:scale-110'/>
                 </div>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8  '>
                 {
                     popularData.map(popularTour => <PopularTour
                         key={popularTour.id}
